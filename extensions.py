a255eebdd3302471d8c8378339aadd53a4345dd2
sobago <isobago@gmail.com>
2022-03-16 16:48:01 +0300

    Initial commit

---
+++ extensions.py
@@ -0,0 +1,37 @@
+import requests
+import json
+from config import keys
+
+
+class APIException(Exception):
+    pass
+
+
+class APIRequest:
+    @staticmethod
+    def conv(base: str, quote: str, amount: str):
+        if base == quote:
+            raise APIException(f'Нельзя конвертировать валюту "{base}" саму в себя...')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту "{base}"')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту "{quote}"')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Количество "{amount}" должно быть числом')
+
+        if amount <= 0:
+            raise APIException(f'Количество должно быть больше нуля!')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+        result = round(float(json.loads(r.content)[keys[quote]]) * amount, 2)
+
+        return result
---
+++ main.py
@@ -0,0 +1,47 @@
+import telebot
+from config import TOKEN, API_KEY, keys
+from extensions import APIException, APIRequest
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help_msg(message: telebot.types.Message):
+    bot.reply_to(message, f'Добро пожаловать, {message.chat.username} 👋🏻\n'
+                          f'Это бот для конвертации валют\n'
+                          f'Формат ввода команды:\n'
+                          f'<валюта, которую конвертируем> <валюта, в которую конвертируем> <количество первой валюты>\n'
+                          f'Например, запрос "доллар рубль 10" - отобразит цену 10 долларов в рублях\n'
+                          f'*Дробный разделитель "точка", округление при расчетах до 2 символов после точки\n'
+                          f'Список доступных валют: /values')
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    res = 'Доступны следующие валюты:\n'
+    for key in keys.keys():
+        res += f'{key}\n'
+    bot.reply_to(message, f'{res}')
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    # долларUSD рубльRUB 1
+    try:
+        vls = message.text.split(' ')
+
+        if len(vls) != 3:
+            raise APIException('Некорректное количество параметров /help')
+
+        base, quote, amount = vls
+        total = APIRequest.conv(base, quote, amount)
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя:\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Возникла ошибка\n{e}')
+    else:
+        bot.reply_to(message, f'{amount} {base} = {total} {quote}')
+
+
+bot.polling(none_stop=True)
